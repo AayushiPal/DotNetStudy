510a140f7bc70992198b0c43af52ec0c46968610
AayushiPal <Aayushipal06gmail.com>
2023-04-02 10:18:36 +0530

    Web API Code added.

--- WebApiStudy/Program.cs
+++ WebApiStudy/Program.cs
@@ -1,12 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiStudy.ModelFolder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+string connectionstring =
+          "Data Source=DESKTOP-2BI55C2;Initial Catalog=IplDb;Integrated Security=True;  TrustServerCertificate=True;";
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<ApplicationDB>
+    (options => options.UseSqlServer(connectionstring));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
